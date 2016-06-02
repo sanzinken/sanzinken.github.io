12ca4655d4261f93e5d4cbd260bdb69a061e60f9
Sasha Z <sanzinken@gmail.com>
2016-06-02 23:25:32 +0300

    fix some problem

--- JS/07_08/js/index.js
+++ JS/07_08/js/index.js
@@ -5,13 +5,12 @@ $(function() {
         $('.wrapper .tab').removeClass('active').addClass('hoverOn').eq($(this).index()).addClass('active').removeClass('hoverOn');
         $('.textblock').hide().eq($(this).index()).fadeIn();
     }).eq(0).addClass('active');
-
-    $('.formblock .form-field').hover(function(){
-        $('.formblock .helptext').eq($('.form-wrapper .form-field').index()).toggleClass('.helptext-active');
+    $('.helptext').hide();
+    $('.form-wrapper .form-field').hover(function(){
+        console.log($('.form-wrapper .form-field').index(this));
+        $('.form-wrapper .helptext').eq($('.form-wrapper .form-field').index(this));
     },
     function(){
-        console.log($('.helptext'));
-        $('.helptext').eq($('.form-wrapper .form-field').index()).toggleClass('.helptext-active');
-        console.log($('.helptext').eq($('.form-wrapper .form-field').index(this)));
+        $('.form-wrapper .helptext').eq($('.form-wrapper .form-field').index(this));
     });
 });
