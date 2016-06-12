057a6ab789f08bece6dc57a630b2806c681dd519
Sasha Z <sanzinken@gmail.com>
2016-06-13 00:00:17 +0300

    carousel work

--- JS/09_10/js/script.js
+++ JS/09_10/js/script.js
@@ -1,5 +1,16 @@
 $(function() {
+
     $('.jcarousel').jcarousel({
         // Configuration goes here
     });
+
+
+     $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel').jcarousel('scroll', '+=1');
 });
