1cb6b7c194828a96103966299d0ad77df9b5073b
Sasha Z <sanzinken@gmail.com>
2016-06-14 22:41:51 +0300

    add selectric

--- JS/09_10/js/script.js
+++ JS/09_10/js/script.js
@@ -12,5 +12,23 @@ $(function() {
         target: '+=1'
     });
 
-    $('.jcarousel').jcarousel('scroll', '+=1');
+    $('.jcarousel').jcarousel('scroll');
+
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function(page) {
+            return '<a href="#' + page + '">' + '</a>';
+        }
+    });
+
+    $('.jcarousel-pagination a').eq(0).addClass('active');
+
+    $('.jcarousel-pagination').on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active');
+    }).on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active');
+    }).on('click', function(e) {
+        e.preventDefault();
+    })
+// Selectric
+    $('select').selectric();
 });
