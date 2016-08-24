cf7d023a5aca18fdbe048f629a4e36ca04cd6811
sanzinken <sanzinken@gmail.com>
2016-08-24 21:11:46 +0300

    Add <div class="carousel-wrapper"> and put all carousel elements in

--- JS/11_12/script/jquery.myCarouselPlugin.js
+++ JS/11_12/script/jquery.myCarouselPlugin.js
@@ -17,16 +17,24 @@
     var $list = $(this);
     var $hider = $('.carousel-hider');
 
+    $hider.after('<div class="carousel-wrapper"></div>');
+    var $carouselWrapper = $('.carousel-wrapper');
+
+
     $img.css({'max-width': settings.imgWidth});
     $listItem.css({'display':'block', 'margin-right': settings.listItemMarginRight, 'float': 'left'});
     $list.css({'width': '1000000px', 'position': 'relative', 'padding': '0px'});
     $hider.css({'width': settings.hiderWidth, 'overflow': 'hidden', 'float': 'left'});
 
     $hider.before('<a href="#" class="left-arrow">Сюда</a>');
-    $('.wrapper').append('<a href="#" class="right-arrow">Туда</a>');
+    $hider.after('<a href="#" class="right-arrow">Туда</a>');
 
     var $leftArrow = $('.left-arrow');
     var $rightArrow = $('.right-arrow');
+
+    $leftArrow.appendTo($carouselWrapper);
+    $hider.appendTo($carouselWrapper);
+    $rightArrow.appendTo($carouselWrapper);
 
     $leftArrow.css({'float': 'left', 'text-decoration': 'none', 'color':'black', 'cursor': 'pointer', 'display': 'block', 'border': '1px solid grey', 'background-color': 'lightgrey', 'padding': '5px', 'margin-top':'25px', 'margin-right': '25px'});
     $rightArrow.css({'float': 'left', 'text-decoration': 'none', 'color':'black', 'cursor': 'pointer', 'display': 'block', 'border': '1px solid grey', 'background-color': 'lightgrey', 'padding': '5px', 'margin-top':'25px', 'margin-left': '25px'});
