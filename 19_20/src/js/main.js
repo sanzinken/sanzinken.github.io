8ce47e4e350bf1b5dae4772b08707c18baa01e22
sanzinken <sanzinken@gmail.com>
2017-06-04 22:56:08 +0300

    done services 19_20

--- 19_20/src/js/main.js
+++ 19_20/src/js/main.js
@@ -1,11 +1,32 @@
 //= partials/jquery.min.js
 //= partials/owl.carousel.js
+//= ../../bower_components/wow/dist/wow.min.js
 
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
-    autoplayTimeout: 2000
+    autoplayTimeout: 2000,
+    responsive:{
+      0:{
+        nav: true,
+        dots: false,
+        touchDrag: true,
+        navText: ['<i class="fa fa-chevron-left chevrons fa-4x" aria-hidden="true"></i>',
+        '<i class="fa fa-chevron-right chevrons fa-4x" aria-hidden="true"></i>']
+      },
+      1200: {
+        nav: false,
+        dots: true
+      }
+    }
   });
+
+  $(".header-button-menu-760").on('click', function() {
+    $('.main-menu').toggle();
+    $(this).toggleClass('header-button-menu-760_active');
+  });
+
+  new WOW().init();
 });
