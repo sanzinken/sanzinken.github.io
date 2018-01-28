f378ac94dd7a6ff95f54eb11a574c1bf0815cfea
sanzinken <sanzinken@gmail.com>
2018-01-28 22:59:16 +0200

    320 done (none pixel perfect)

--- exam/src/js/main.js
+++ exam/src/js/main.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 
     $('.item__slider').slick({
         adaptiveHeight: true,
-        autoplay: true
+        autoplay: true,
+        responsive: [{
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
+        }]
     });
 
     // var xhr = new XMLHttpRequest();
